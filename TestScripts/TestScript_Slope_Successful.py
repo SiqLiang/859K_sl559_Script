76aa06664237f957750772ce16b245826965646b
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-11-27 15:55:38 +0800

    Update TestScript_Slope_Successful.py

--- TestScripts/TestScript_Slope_Successful.py
+++ TestScripts/TestScript_Slope_Successful.py
@@ -2,4 +2,4 @@ import arcpy
 from arcpy.sa import *
 arcpy.env.workspace = "C:\\859K_sl559\\Data"
 outRas = Slope("Area_mask_DEM90m_final.tif")
-outRas.save("C:\\859K_sl559\\Data\\SlopeTest1.tif")
+outRas.save("SlopeTest2.tif")
