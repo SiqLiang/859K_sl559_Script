db1ccbba3fd38df66c8b0f4c421e62091558eace
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-11-27 20:56:32 +0800

    update GCS and PCS

--- test.py
+++ test.py
@@ -11,6 +11,9 @@ arcpy.env.extent = "C:\\859K_sl559\\Data\\RegionOfInterest.shp"
 inputShapefile = "C:\\859K_sl559\\Data\\EndemicBirdSpecies_inROI_final.shp"
 DEM_roi = "C:\\859K_sl559\\Data\\Area_mask_DEM90m_final.tif"
 
+#setting GCS and PCS
+arcpy.env.outputCoordinateSystem = arcpy.SpatialReference(inputShapefile)
+
 # Create a cursor (this is like opening a file)
 rows = arcpy.da.SearchCursor(inputShapefile,['FID', "Scientific", "Min", "Max"]) 
 row = rows.next()
@@ -60,11 +63,11 @@ for i in range (0,83,1):
     
     #BL_Map Refined by species elevation range
     ReBy_DEM = Raster(BL_Map)*Raster(ConDEM)
-    outRas.save("ReBy_DEM_"+scientific_name+".tif")
-    
+    ReBy_DEM.save("ReBy_DEM_"+scientific_name+".tif")
     
     row = rows.next()
-del rows
+del rows     
+
 
 
 
