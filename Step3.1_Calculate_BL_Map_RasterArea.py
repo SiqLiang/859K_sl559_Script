ae779c24ff77faae8b2645786e6137e17ac34794
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-11-29 11:34:48 +0800

    Update Step3.1_Calculate_BL_Map_RasterArea.py

--- Step3.1_Calculate_BL_Map_RasterArea.py
+++ Step3.1_Calculate_BL_Map_RasterArea.py
@@ -8,21 +8,38 @@ from arcpy.sa import *
 arcpy.env.overwriteOutput = True
 arcpy.env.workspace = "C:\\859K_sl559\\Scaratch1_1_BL_Map_rasters_and_doc"
 
-fileList = arcpy.ListRasters('BL_Map_*', 'All')
-len(fileList)
-
-# print the current working directory
-print (os.getcwd())
-# Create a new file in the current working directory, write some text, and close it
-BL_Map_fileObj = open("BL_Map_RasterArea.txt",'w')
+#print the current working directory
+#print (os.getcwd())
+#Check the existence of the target txt file
 print (os.path.exists("C:\\859K_sl559\\Scaratch1_1_BL_Map_rasters_and_doc\\BL_Map_RasterArea.txt"))
+# Create a new file in the current working directory, write some text, and remember to close it
+BL_Map_fileObj = open("BL_Map_RasterArea.txt",'w')
+BL_Map_fileObj.truncate(0) # clear eveything already in the txt file
+BL_Map_fileObj.write('Species, '+'Area'+"\n")
+#BL_Map_fileObj.close()
+
 # create constant and loop variables.
 # for projection World_Eckert_IV
 cellsize = 94.126759784775*94.126759784775/1000000
 #cellsize= 94.126759784775m*94.126759784775m/1000000 = 0.0088598469075807 square km2
-BL_Map_fileObj.write('Species, '+'Area'+"\n")
 
-#fileList = arcpy.ListRasters('BL_Map_*')
+fileList = arcpy.ListRasters('BL_Map_*', 'All')
+len(fileList)
+fileName = fileList.Next()
+
+
+while fileList:
+    print(fileName)
+    scientific_name = fileName[7:fileName.rfind('.')]
+    # Create a search cursor for desired raster VALUE, extract COUNT and multiply by cellsize to get area.
+    sCur = arcpy.da.SearchCursor(fileName, "Value", '"Value" = 1')
+    for row in sCur:
+        cellCount = row.getValue("Count")
+        area = cellCount*cellsize
+        BL_Map_fileObj.write(str(scientific_name)+', '+str(area)+"\n")
+        fileName = fileList.Next()
+
+
 
 #for fileName in fileList:
     #print(fileName)
