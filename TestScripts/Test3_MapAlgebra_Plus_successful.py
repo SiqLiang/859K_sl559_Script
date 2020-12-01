298fe1db4b406c7f33fa7a7cd8cae10b5cb9a6ac
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-12-01 20:33:56 +0800

    Delete Test3_MapAlgebra_Plus_successful.py

--- TestScripts/Test3_MapAlgebra_Plus_successful.py
+++
@@ -1,6 +0,0 @@
-import arcpy
-from arcpy.sa import *
-arcpy.env.workspace = "C:\\859K_sl559\\Data"
-outRas = Raster("WCountriesG_B50_rC.tif") + Raster("SumPA_rC.tif")
-outRas.save("Test3_MapAlgebra_Plus.tif")
-
