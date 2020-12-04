f825f60fd58418bcc991038c9f596defc2028569
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-12-04 15:21:35 +0800

    Update Step3.17_RegionGroup_ReClassNoData_H_TC2000.py

--- Step3.17_RegionGroup_ReClassNoData_H_TC2000.py
+++ Step3.17_RegionGroup_ReClassNoData_H_TC2000.py
@@ -5,12 +5,15 @@ import os
 arcpy.env.overwriteOutput = True
 arcpy.env.workspace = "C:\\859K_sl559\\Scratch3"
 arcpy.env.extent = "C:\\859K_sl559\\Scratch3\\ReclassNoData_H_TC2000.tif"
+print (os.path.exists("C:\\859K_sl559\\Scratch3\\ReclassNoData_H_TC2000.tif"))
 
 # Local variables:
 ReclassNoData_H_TC2000_tif = "C:\\859K_sl559\\Scratch3\\ReclassNoData_H_TC2000.tif"
-RegionG_rCNoDataTC2000_test1_tif = "C:\\859K_sl559\\Scratch3\\RegionG_rCNoDataTC2000_test1.tif"
+RegionG_rCNoDataTC2000_test2_tif = "C:\\859K_sl559\\Scratch3\\RegionG_rCNoDataTC2000_test2.tif"
 
 # Process: Region Group
-arcpy.gp.RegionGroup_sa(ReclassNoData_H_TC2000_tif, RegionG_rCNoDataTC2000_test1_tif, "FOUR", "WITHIN", "ADD_LINK", "")
+arcpy.gp.RegionGroup_sa(ReclassNoData_H_TC2000_tif, RegionG_rCNoDataTC2000_test2_tif, "FOUR", "WITHIN", "ADD_LINK", "")
 
-print (os.path.exists("C:\\859K_sl559\\Scratch3\\RegionG_rCNoDataTC2000_test1.tif"))
+print (os.path.exists("C:\\859K_sl559\\Scratch3\\RegionG_rCNoDataTC2000_test2.tif"))
+
+
