7a046dff83a42b60137ba40a604ee0461a749c8a
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-12-04 22:40:14 +0800

    Update RefionG2_rCRGrCndTC2000.py

--- RefionG2_rCRGrCndTC2000.py
+++ RefionG2_rCRGrCndTC2000.py
@@ -3,10 +3,11 @@
 Created on Fri Dec  4 22:20:29 2020
 
 @author: sl559
-I have regiongrouped the ReclassNoData_H_TC2000.tif file, and classified the clusters with 
-cellsize smaller than 113 to nodata. Now, I run the 2nd regiongroup to sign a name to each 
-cluster.
-
+I have regiongrouped the ReclassNoData_H_TC2000.tif file to
+RegionG_rCNoDataTC2000_test2.tif, and classified the clusters with 
+cellsize smaller than 113 to nodata, the result is
+Reclass_RegionG_rCNTC2000.tif. 
+Now, I run the 2nd regiongroup to sign a sequantial name to each cluster.
 """
 #2020Dec4
 # Import arcpy module
@@ -16,11 +17,11 @@ arcpy.env.overwriteOutput = True
 arcpy.env.workspace = "C:\\859K_sl559\\Scratch3"
 arcpy.env.extent = "C:\\859K_sl559\\Scratch3\\ReclassNoData_H_TC2000.tif"
 print (os.path.exists("C:\\859K_sl559\\Scratch3\\ReclassNoData_H_TC2000.tif"))
-
+print (os.path.exists("C:\\859K_sl559\\Scratch3\\Reclass_RegionG_rCNTC2000.tif"))
 # Local variables:
-RegionG_rCNoDataTC2000_test2_tif = "C:\\859K_sl559\\Scratch3\\RegionG_rCNoDataTC2000_test2.tif"
-RefionG2_rCRGrCndTC2000_tif = "C:\\859K_sl559\\Scratch3\\RefionG2_rCRGrCndTC2000.tif"
+Reclass_RegionG_rCNTC2000_tif = "C:\\859K_sl559\\Scratch3\\Reclass_RegionG_rCNTC2000.tif"
+RegionG2_rCRGrCndTC2000_tif = "C:\\859K_sl559\\Scratch3\\RegionG2_rCRGrCndTC2000.tif"
 # Process: Region Group
-arcpy.gp.RegionGroup_sa(RegionG_rCNoDataTC2000_test2_tif, RefionG2_rCRGrCndTC2000_tif, "FOUR", "WITHIN", "ADD_LINK", "")
+arcpy.gp.RegionGroup_sa(Reclass_RegionG_rCNTC2000_tif, RegionG2_rCRGrCndTC2000_tif, "FOUR", "WITHIN", "ADD_LINK", "")
 
-print (os.path.exists("C:\\859K_sl559\\Scratch3\\RefionG2_rCRGrCndTC2000.tif"))
+print (os.path.exists("C:\\859K_sl559\\Scratch3\\RefionG2_rCRGrCndTC2000_tif"))
