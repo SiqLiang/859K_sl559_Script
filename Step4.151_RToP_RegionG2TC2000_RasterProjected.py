e16417ea975f4861bf5e82e2ae4059e05e9a037b
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-12-06 18:59:19 +0800

    Update Step4.151_RToP_RegionG2TC2000_RasterProjected.py

--- Step4.151_RToP_RegionG2TC2000_RasterProjected.py
+++ Step4.151_RToP_RegionG2TC2000_RasterProjected.py
@@ -1,6 +1,8 @@
 #Before I calculate the area and perimeter of each forest patch,
 #I need to project the tif into World_Eckert_IV
 
+###########Unsuccessful
+
 # Import arcpy module
 import arcpy
 import os
@@ -19,3 +21,7 @@ RegionG2TC2000_RasterProjected_tif= "C:\\859K_sl559\\Scratch3\\RegionG2TC2000_Ra
 arcpy.ProjectRaster_management(RegionG2_rCRGrCndTC2000_tif, RegionG2TC2000_RasterProjected_tif, "PROJCS['World_Eckert_IV',GEOGCS['GCS_WGS_1984',DATUM['D_WGS_1984',SPHEROID['WGS_1984',6378137.0,298.257223563]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Eckert_IV'],PARAMETER['False_Easting',0.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',0.0],UNIT['Meter',1.0]]", "NEAREST", "94.1231502405956 94.1231502405956", "", "", "GEOGCS['GCS_WGS_1984',DATUM['D_WGS_1984',SPHEROID['WGS_1984',6378137.0,298.257223563]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]")
 
 print (os.path.exists("C:\\859K_sl559\\Scratch3\\RegionG2TC2000_RasterProjected.tif"))
+
+#ExecuteError: Failed to execute. Parameters are not valid.
+#Undefined coordinate system for input dataset.
+#Failed to execute (ProjectRaster).
