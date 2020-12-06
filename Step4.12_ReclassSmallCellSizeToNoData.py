5dfa0de2c610b54924c4349bf00bed4ab369ef88
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-12-06 16:10:29 +0800

    Update Step4.12_ReclassSmallCellSizeToNoData.py

--- Step4.12_ReclassSmallCellSizeToNoData.py
+++ Step4.12_ReclassSmallCellSizeToNoData.py
@@ -1,1 +1,19 @@
 # The patch with cellsize smaller than 113 i.e. 100 hacter were reclassified into NoData in arcgis pro
+import arcpy
+from arcpy.sa import *
+
+# allow overwrite
+arcpy.env.overwriteOutput = True
+
+# Set the analysis environments
+arcpy.env.workspace = "C:\\859K_sl559\\Scratch3"
+arcpy.env.extent = "C:\\859K_sl559\\Data\\RegionOfInterest.shp"
+
+RegionG_rCNoDataTC2000_test2_tif = "C:\\859K_sl559\\Scratch3\\RegionG_rCNoDataTC2000_test2.tif"
+Reclass_RegionG_rCNTC2000_tif = "C:\\859K_sl559\\Scratch3\\Reclass_RegionG_rCNTC2000.tif"
+
+#Execute Reclassify 
+reclassField = "Count"
+remap = "1 112 NODATA; NODATA NODATA"
+BL_Map= "BL_Map_"+scientific_name+".tif"
+arcpy.gp.Reclassify_sa(RegionG_rCNoDataTC2000_test2_tif, reclassField, remap, Reclass_RegionG_rCNTC2000_tif)
