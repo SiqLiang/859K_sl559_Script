3732d099f7bf401caf984f6b9c7ccb9f873e30a3
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-12-06 16:10:24 +0800

    Update Step4.15_RToP_RegionG2TC2000_Projected.py

--- Step4.15_RToP_RegionG2TC2000_Projected.py
+++ Step4.15_RToP_RegionG2TC2000_Projected.py
@@ -14,8 +14,8 @@ print (os.path.exists("C:\\859K_sl559\\Scratch3\\RasterToPolygon_RegionG2TC2000.
 
 # Local variables:
 RasterToPolygon_RegionG2TC2000_shp = "C:\\859K_sl559\\Scratch3\\RasterToPolygon_RegionG2TC2000.shp"
-RToP_RegionG2TC2000_Projected_shp= "C:\\859K_sl559\\Scratch3\\RToP_RegionG2TC2000_Projected.shp"
+RToP_RegionG2TC2000_Projected1_shp= "C:\\859K_sl559\\Scratch3\\RToP_RegionG2TC2000_Projected1.shp"
 # Process: Project
-arcpy.Project_management(RasterToPolygon_RegionG2TC2000_shp, RToP_RegionG2TC2000_Projected_shp, "PROJCS['World_Eckert_IV',GEOGCS['GCS_WGS_1984',DATUM['D_WGS_1984',SPHEROID['WGS_1984',6378137.0,298.257223563]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Eckert_IV'],PARAMETER['False_Easting',0.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',0.0],UNIT['Meter',1.0]]", "", "GEOGCS['GCS_WGS_1984',DATUM['D_WGS_1984',SPHEROID['WGS_1984',6378137.0,298.257223563]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]", "NO_PRESERVE_SHAPE", "", "NO_VERTICAL")
+arcpy.Project_management(RasterToPolygon_RegionG2TC2000_shp, RToP_RegionG2TC2000_Projected1_shp, "PROJCS['World_Eckert_IV',GEOGCS['GCS_WGS_1984',DATUM['D_WGS_1984',SPHEROID['WGS_1984',6378137.0,298.257223563]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Eckert_IV'],PARAMETER['False_Easting',0.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',0.0],UNIT['Meter',1.0]]", "", "GEOGCS['GCS_WGS_1984',DATUM['D_WGS_1984',SPHEROID['WGS_1984',6378137.0,298.257223563]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]", "NO_PRESERVE_SHAPE", "", "NO_VERTICAL")
 
 
