48f9aa179b2b42ead4544fa58b22c3dc485145f0
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-12-07 14:15:46 +0800

    Create Forest_EucDist_reC_RegionG2_TC2000_Proj_tif.py

---
+++ Forest_EucDist_reC_RegionG2_TC2000_Proj_tif.py
@@ -0,0 +1,20 @@
+#Because the Edistance Map was based on the area with no forest, The area with no forest was conted 
+#distance too. I use the forest map (reC2_RegionG2_TC2000_Proj_tif) to mask out the forest area with EDistance
+
+import arcpy
+from arcpy.sa import *
+
+# allow overwrite
+arcpy.env.overwriteOutput = True
+
+# Set the analysis environments
+arcpy.env.workspace = "C:\\859K_sl559\\Scratch3"
+arcpy.env.extent = "C:\\859K_sl559\\Data\\RegionOfInterest.shp"
+
+# Local variables:
+EucDist_reC_RegionG2_TC2000_Proj_tif = "C:\\859K_sl559\\Scratch3\\EucDist_reC_RegionG2_TC2000_Proj.tif"
+reC2_RegionG2_TC2000_Proj_tif= "C:\\859K_sl559\\Scratch3\\reC2_RegionG2_TC2000_Proj.tif"
+
+# Maskout the forest area from the Edistance Map
+outRas = Raster(EucDist_reC_RegionG2_TC2000_Proj_tif)*Raster(reC2_RegionG2_TC2000_Proj_tif)
+outRas.save(Forest_EucDist_reC_RegionG2_TC2000_Proj_tif)
