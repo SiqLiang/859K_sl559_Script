3168a2eeea5b05ef1022988e52c4a042c3cc833e
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-12-07 21:12:24 +0800

    Update use_os_to_deleted_unwanted_csv.py

--- use_os_to_deleted_unwanted_csv.py
+++ use_os_to_deleted_unwanted_csv.py
@@ -1,19 +1,21 @@
 import os
 print(os.getcwd())
-path= "C:\\859K_sl559\\Data\\ModisFire2001_2019\\2001"
-os.chdir(path)
-print(os.getcwd())
-os.listdir(path)
-len(os.listdir(path))
-WantedFilelist= ["Bangladesh", "Bhutan", "China", "India", "Laos", "Myanmar", "Nepal", "Thailand", "Vietnam"]
-filelist= os.listdir(path)
-file = filelist.next()
-for file in os.listdir(path):
-    print(file)
-    Country_name = file[11:file.rfind('.')]
-    print(Country_name)
-    if Country_name in WantedFilelist:
-        print("This {} fire dataset is selected".format(Country_name))
-    else:
-        os.remove(file)
-        file = filelist.next()
+bathpath="C:\\859K_sl559\\Data\\ModisFire2001_2019"
+yearlist=["2001","2002","2003","2004","2005","2006","2007","2008","2009","2010",
+          "2011","2012","2013","2014","2015","2016","2017","2018","2019"]
+for year in yearlist:
+    path= os.path.join(bathpath,year) 
+    os.chdir(path)
+    print(os.getcwd())
+    os.listdir(path)
+    print(len(os.listdir(path)))
+    WantedFilelist= ["Bangladesh", "Bhutan", "China", "India", "Laos", "Myanmar", "Nepal", "Thailand", "Vietnam"]
+    filelist= os.listdir(path)
+    for file in filelist:
+        print(file)
+        Country_name = file[11:file.rfind('.')]
+        print(Country_name)
+        if Country_name in WantedFilelist:
+            print("This {} fire dataset is selected".format(Country_name))
+        else:
+            os.remove(file)
