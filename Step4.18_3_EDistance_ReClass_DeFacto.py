f2f7a867c71c866d8df4d7453a9cc480776c720a
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-12-11 22:22:09 +0800

    Create Step4.18_3_EDistance_ReClass_DeFacto.py

---
+++ Step4.18_3_EDistance_ReClass_DeFacto.py
@@ -0,0 +1,14 @@
+
+import arcpy
+arcpy.env.overwriteOutput = True
+arcpy.env.workspace = "C:\\859K_sl559\\Scratch3"
+arcpy.env.extent = "C:\\859K_sl559\\Data\\RegionOfInterest.shp"
+
+# Local variables:
+EucDist_reC_RegionG2_TC2000_Proj_tif = "EucDist_reC_RegionG2_TC2000_Proj.tif"
+DeFacto_EucDist_reC_RegionG2_TC2000_Proj_tif = "C:\\859K_sl559\\Scratch3\\DeFacto_EucDist_reC_RegionG2_TC2000_Proj.tif"
+
+# Process: Reclassify
+arcpy.gp.Reclassify_sa(EucDist_reC_RegionG2_TC2000_Proj_tif, "VALUE", "0 3000 NODATA;3000 13604 1", DeFacto_EucDist_reC_RegionG2_TC2000_Proj_tif, "DATA")
+
+ 
