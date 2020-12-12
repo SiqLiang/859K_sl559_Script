ddce23b2034b089ee7e4abc80ccf545bb96dabc1
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-12-12 22:26:17 +0800

    Create Python_LayerToPolygon.py

---
+++ Python_LayerToPolygon.py
@@ -0,0 +1,24 @@
+import arcpy
+import os
+
+arcpy.env.workspace = "C:\\859K_sl559\\Scratch5"
+
+# Set Local Variables
+composite = 'NO_COMPOSITE'
+pixels = 2048
+dpi = 96
+clamped = 'CLAMPED_TO_GROUND'
+
+# Use the ListFiles method to identify all lyr and lyrx files in workspace
+layers = arcpy.ListFiles("*.shp*") 
+
+if len(layers) > 0:
+    for layer in layers:        
+        # Strips the '.lyr(x)' part of the name and appends '.kmz'
+        outKML =  "test1"+".kmz"
+        for scale in range(10000, 30001, 10000):
+            # Execute LayerToKML
+            arcpy.LayerToKML_conversion(layer, outKML, scale, composite, 
+                                        '', pixels, dpi, clamped)
+else:
+    arcpy.AddMessage('There are no layer files in {}'.format(arcpy.env.workspace))
