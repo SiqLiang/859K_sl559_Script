4524b4ad6fd0154a405d6e407f825acc220dd3ab
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-12-12 20:56:18 +0800

    Update Step5.12_RelassRefinedSP_RasterToPolygon.py

--- Step5.12_RelassRefinedSP_RasterToPolygon.py
+++ Step5.12_RelassRefinedSP_RasterToPolygon.py
@@ -1,20 +1,22 @@
-# Import arcpy module
 import arcpy
+from arcpy.sa import *
 arcpy.env.overwriteOutput = True
-arcpy.env.workspace = "C:\\859K_sl559\\Scratch1"
-
-
-# Local variables:
-ReBy_DEMFC30_Zoothera_salimalii_tif = "ReBy_DEMFC30_Zoothera_salimalii.tif"
-RcReBy_DEMFC30_Zoothera_salimalii_tif= "RcReBy_DEMFC30_Zoothera_salimalii.tif"
-
-
-arcpy.gp.Reclassify_sa(ReBy_DEMFC30_Zoothera_salimalii_tif, "VALUE", 
+arcpy.env.outputCoordinateSystem = arcpy.SpatialReference("WGS 1984")
+arcpy.env.workspace = "C:\\859K_sl559\\Scratch4"
+arcpy.env.extent = "C:\\859K_sl559\\Data\\RegionOfInterest.shp"
+#
+fileList = arcpy.ListRasters('ReBy_DEMFC30_*', 'All')
+len(fileList) #Check how many Reclassified rasters are avaiable now
+for fileName in fileList:
+    print(fileName)
+    scientific_name = fileName[13:fileName.rfind('.')]
+    outRas= "RcReBy_DEMFC30_"+scientific_name+".tif"
+    arcpy.gp.Reclassify_sa(fileName, "VALUE", 
                        "0 NODATA;1 1;NODATA NODATA", 
-                       RcReBy_DEMFC30_Zoothera_salimalii_tif, "DATA")
+                       outRas, "DATA")
+    print(outRas)
+    outShp="DEMFC30_"+scientific_name+".shp"
+    arcpy.RasterToPolygon_conversion(outRas, outShp, "NO_SIMPLIFY", "Value")
+    print(outShp)
 
-RtP_RcReBy_DEMFC30_Zoothera_salimalii_shp = "RtP_RcReBy_DEMFC30_Zoothera_salimalii.shp"
-
-# Process: Raster to Polygon
-arcpy.RasterToPolygon_conversion(RcReBy_DEMFC30_Zoothera_salimalii_tif, RtP_RcReBy_DEMFC30_Zoothera_salimalii_shp, "NO_SIMPLIFY", "Value")
-
+    
