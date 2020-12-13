d9260750f2a1fcb5add37ad2a5dde718dac29f9d
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-12-13 12:27:15 +0800

    Update Step5.13_PrintCodetoRunRasterToKML.py

--- Step5.13_PrintCodetoRunRasterToKML.py
+++ Step5.13_PrintCodetoRunRasterToKML.py
@@ -2,7 +2,7 @@ import arcpy
 from arcpy.sa import *
 arcpy.env.overwriteOutput = True
 arcpy.env.outputCoordinateSystem = arcpy.SpatialReference("WGS 1984")
-arcpy.env.workspace = "C:\\859K_sl559\\Scratch4"
+arcpy.env.workspace = "C:\\859K_sl559\\Scratch5"
 arcpy.env.extent = "C:\\859K_sl559\\Data\\RegionOfInterest.shp"
 #
 print("import arcpy")
@@ -14,3 +14,6 @@ for fileName in fileList:
     output_kmz= "C:/859K_sl559/Scratch5/"+"DEMFC30_"+scientific_name+".kmz"
     print("arcpy.LayerToKML_conversion(\"{0}\",\"{1}\", \"0\", \"NO_COMPOSITE\", \" \", \"2048\", \"96\", \"CLAMPED_TO_GROUND\")".format(input_raster,output_kmz))
 ###AndThen Run these code in the python window in arcMap.These code can run in arcpro too, but all the kml in black instead of with different colors.
+
+fileList = arcpy.ListFiles('*.kmz')
+len(fileList)
