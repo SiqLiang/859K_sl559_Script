7195fd8e1e6a2cce8161177f1a32c742911cfa04
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-12-15 22:07:05 +0800

    Update Step1.14_DeJurePA_PolygonToRaster.py

--- Step1.14_DeJurePA_PolygonToRaster.py
+++ Step1.14_DeJurePA_PolygonToRaster.py
@@ -5,16 +5,16 @@ from arcpy.sa import *
 arcpy.env.overwriteOutput = True
 
 # Set the analysis environments
-arcpy.env.workspace = "C:\\859K_sl559\\Data\\Dejure_PA"
+arcpy.env.workspace = "C:\\859K_sl559\\Data\\Dejure_PA\\DejurePA.gdb"
 arcpy.env.extent = "C:\\859K_sl559\\Data\\RegionOfInterest.shp"
 arcpy.env.outputCoordinateSystem = arcpy.SpatialReference("WGS 1984")
 #input datasets
-inputShapefile = "C:\\859K_sl559\\Data\\Dejure_PA\\WDPApolygon_ChinaPA_roiClip_Merge.shp"
+inputShapefile = "DejurePA_2020Dec12"
 
 #polygon to raster
-BLraw = "rawDejure_2020Dec12" + ".tif"
+raster_raw = "rawDejure_2020Dec12"
 cellSize = 0.0008333
-field = "RasterValue"
-arcpy.FeatureToRaster_conversion(inputShapefile, field, BLraw, cellSize)
+field = "RasterValu"
+arcpy.FeatureToRaster_conversion(inputShapefile, field, raster_raw, cellSize)
 
 
