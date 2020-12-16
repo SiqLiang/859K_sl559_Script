e86d08ac9df746a2b9e334536540a4b33b510422
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-12-16 17:00:31 +0800

    Create Step2.13_produce_DEMFC30_DeJure.py

---
+++ Step2.13_produce_DEMFC30_DeJure.py
@@ -0,0 +1,23 @@
+# This script aims at run raster claculator times to 
+#produce DEMFC30_PA_+scientific_name+".tif"
+
+#PA means De-jure in this script
+import arcpy
+from arcpy.sa import *
+arcpy.env.overwriteOutput = True
+arcpy.env.outputCoordinateSystem = arcpy.SpatialReference("WGS 1984")
+arcpy.env.workspace = "C:\\859K_sl559\\Scratch1"
+arcpy.env.extent = "C:\\859K_sl559\\Data\\RegionOfInterest.shp"
+Dejure_roi= "C:\\859K_sl559\\Data\Dejure_PA\\Classified_Dejure_PA_2020Dec16.tif"
+
+       
+sumRas = 0 # set init value and then loop through file list
+fileList = arcpy.ListRasters('ReBy_DEMFC30_*', 'All')
+len(fileList) #Check how many Reclassified rasters are avaiable now
+for fileName in fileList:
+    print(fileName)
+    scientific_name = fileName[13:fileName.rfind('.')]
+    outRas = Raster(fileName)*Raster(Dejure_roi)
+    outRas.save("DEMFC30_DeJure_"+scientific_name+".tif")
+    print(outRas)
+    
