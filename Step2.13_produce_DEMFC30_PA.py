ec0fdbd74386edce43a423edaabcc1b695645baa
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-12-16 16:01:25 +0800

    Update Step2.13_produce_DEMFC30_PA.py

--- Step2.13_produce_DEMFC30_PA.py
+++ Step2.13_produce_DEMFC30_PA.py
@@ -4,7 +4,7 @@ import arcpy
 from arcpy.sa import *
 arcpy.env.overwriteOutput = True
 arcpy.env.outputCoordinateSystem = arcpy.SpatialReference("WGS 1984")
-arcpy.env.workspace = "C:\\859K_sl559\\Scratch1"
+arcpy.env.workspace = "C:\859K_sl559\Data\Dejure_PA\Dejure_PA_RawTif"
 arcpy.env.extent = "C:\\859K_sl559\\Data\\RegionOfInterest.shp"
 
 PA_roi= "C:\\859K_sl559\\Data\\SumPA_rC.tif"
@@ -15,10 +15,10 @@ len(fileList) #Check how many Reclassified rasters are avaiable now
 for fileName in fileList:
     print(fileName)
     scientific_name = fileName[13:fileName.rfind('.')]
-    outRas = Raster(fileName)*Raster(PA_roi)
-    outRas.save("DEMFC30_PA_"+scientific_name+".tif")
+    reclassField = "Value"
+    remap = "1 1; NODATA 0"
+    outRas= "DEMFC30_PA_"+scientific_name+".tif"
+    arcpy.gp.Reclassify_sa(fileName, reclassField, remap, outRas)
     print(outRas)
 
 
-layers = arcpy.ListFiles("*.shp") 
-len(layers) 
