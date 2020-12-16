52a351132b7be44e4fe9d8facb0d25e5d6c20dd8
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-12-16 16:50:00 +0800

    Update Step2.13_produce_DEMFC30_PA.py

--- Step2.13_produce_DEMFC30_PA.py
+++ Step2.13_produce_DEMFC30_PA.py
@@ -1,12 +1,15 @@
 # This script aims at run raster claculator times to 
 #produce DEMFC30_PA_+scientific_name+".tif"
+
+#PA means De-jure in this script
 import arcpy
 from arcpy.sa import *
 arcpy.env.overwriteOutput = True
 arcpy.env.outputCoordinateSystem = arcpy.SpatialReference("WGS 1984")
 arcpy.env.workspace = "C:\\859K_sl559\\Scratch1"
 arcpy.env.extent = "C:\\859K_sl559\\Data\\RegionOfInterest.shp"
-PA_roi= "C:\\859K_sl559\\Data\\SumPA_rC.tif"
+PA_roi= "C:\\859K_sl559\\Data\Dejure_PA\\Classified_Dejure_PA_2020Dec16.tif"
+
        
 sumRas = 0 # set init value and then loop through file list
 fileList = arcpy.ListRasters('ReBy_DEMFC30_*', 'All')
