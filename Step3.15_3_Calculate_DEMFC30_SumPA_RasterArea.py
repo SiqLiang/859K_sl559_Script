179000a6412ce2cd4f998d4e173ab55c2fabe68b
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-12-16 19:32:12 +0800

    Create Step3.15_3_Calculate_DEMFC30_SumPA_RasterArea.py

---
+++ Step3.15_3_Calculate_DEMFC30_SumPA_RasterArea.py
@@ -0,0 +1,44 @@
+import os
+from arcpy.sa import *
+arcpy.env.overwriteOutput = True
+arcpy.env.workspace = "C:\\859K_sl559\\Scratch1"
+os.chdir("C:\\859K_sl559\\Doc")
+
+#print the current working directory
+#print (os.getcwd())
+#Check the existence of the target txt file
+print (os.path.exists("C:\\859K_sl559\\Doc\\DEMFC30_SumPA_RasterArea.txt"))
+# Create a new file in the current working directory, write some text, and remember to close it
+BL_Map_fileObj = open("DEMFC30_SumPA_RasterArea.txt",'w')
+BL_Map_fileObj.truncate(0) # clear eveything already in the txt file
+BL_Map_fileObj.write('DEMFC30_SumPA_Species, '+'Area_sqkm2'+"\n")
+#BL_Map_fileObj.close()
+
+# create constant and loop variables.
+# for projection World_Eckert_IV
+cellsize = 94.126759784775*94.126759784775/1000000
+print(cellsize)
+#cellsize= 94.126759784775m*94.126759784775m/1000000 = 0.0088598469075807 square km2
+
+fileList = arcpy.ListRasters('DEMFC30_SumPA_*', 'All')
+print(len(fileList))
+for fileName in fileList:
+    print(fileName)
+    scientific_name = fileName[14:fileName.rfind('.')]
+    print(scientific_name)
+   # Create a search cursor for desired raster VALUE, extract COUNT and multiply by cellsize to get area.
+    rows = arcpy.da.SearchCursor(fileName, ["Value", "Count"],'"VALUE" = 1')
+    try:
+        row=rows.next()
+        cellcount= row[1]
+        print(cellcount)
+        area = cellcount*cellsize
+        print(area)
+        BL_Map_fileObj.write(str(scientific_name)+', '+str(area)+"\n")
+    except Exception:
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~") #a note to show there is no value=1
+        BL_Map_fileObj.write(str(scientific_name)+', '+"0"+"\n") 
+                
+BL_Map_fileObj.close()
+
+
