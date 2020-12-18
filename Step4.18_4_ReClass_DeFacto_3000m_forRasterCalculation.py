fe00e2b6b945a508d3fe28857acaf3a5936b7cd9
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-12-18 15:22:40 +0800

    Update Step4.18_4_ReClass_DeFacto_3000m_forRasterCalculation.py

--- Step4.18_4_ReClass_DeFacto_3000m_forRasterCalculation.py
+++ Step4.18_4_ReClass_DeFacto_3000m_forRasterCalculation.py
@@ -5,10 +5,14 @@ arcpy.env.extent = "C:\\859K_sl559\\Data\\RegionOfInterest.shp"
 arcpy.env.outputCoordinateSystem ="C:\\859K_sl559\\Data\\RegionOfInterest.shp"
 
 # Local variables:
-cellsize = "‪C:\859K_sl559\Data\Area_mask_DEM90m_final.tif"
-DeFacto_EucDist_reC_RegionG2_TC2000_Proj_tif = "DeFacto_EucDist_reC_RegionG2_TC2000_Proj.tif"
-Reclassified_DeFacto_3000m_tif= "Reclassified_DeFacto_3000m.tif"
+in_shp = "Dissolved_DeFacto_3000m.shp"
+PtR_DeFacto_3000m_tif= "PtR_DeFacto_3000m.tif"
+
+cellsize = 0.00083333333
+field = "gridcode"
+arcpy.FeatureToRaster_conversion(in_shp, field, PtR_DeFacto_3000m_tif, cellsize)
 
 # Process: Reclassify
-arcpy.gp.Reclassify_sa(DeFacto_EucDist_reC_RegionG2_TC2000_Proj_tif, "VALUE", "1 1;NODATA 0", Reclassified_DeFacto_3000m_tif, "DATA")
+Reclassified_PtR_DeFacto_3000m_tif= "Reclassified_PtR_DeFacto_3000m.tif"
+arcpy.gp.Reclassify_sa(PtR_DeFacto_3000m_tif, "VALUE", "1 1;NODATA 0", Reclassified_PtR_DeFacto_3000m_tif, "DATA")
 
