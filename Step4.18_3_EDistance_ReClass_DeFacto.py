6f3d35be5911558c7d0e0cc25b54044ae38251ec
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-12-20 21:57:04 +0800

    Update Step4.18_3_EDistance_ReClass_DeFacto.py

--- Step4.18_3_EDistance_ReClass_DeFacto.py
+++ Step4.18_3_EDistance_ReClass_DeFacto.py
@@ -6,9 +6,15 @@ arcpy.env.extent = "C:\\859K_sl559\\Data\\RegionOfInterest.shp"
 arcpy.env.outputCoordinateSystem ="EucDist_reC_RegionG2_TC2000_Proj.tif"
 
 # Local variables:
+#EucDist_reC_RegionG2_TC2000_Proj_tif = "EucDist_reC_RegionG2_TC2000_Proj.tif"
+#DeFacto_EucDist_reC_RegionG2_TC2000_Proj_tif = "C:\\859K_sl559\\Scratch3\\DeFacto_EucDist_reC_RegionG2_TC2000_Proj.tif"
+
+#Process: Reclassify 3000m as threshold
+#arcpy.gp.Reclassify_sa(EucDist_reC_RegionG2_TC2000_Proj_tif, "VALUE", "0 3000 NODATA;3000 13604 1", DeFacto_EucDist_reC_RegionG2_TC2000_Proj_tif, "DATA")
+
+
 EucDist_reC_RegionG2_TC2000_Proj_tif = "EucDist_reC_RegionG2_TC2000_Proj.tif"
-DeFacto_EucDist_reC_RegionG2_TC2000_Proj_tif = "C:\\859K_sl559\\Scratch3\\DeFacto_EucDist_reC_RegionG2_TC2000_Proj.tif"
+DeFacto_1000m_TC2000_90m_Proj_tif = "C:\\859K_sl559\\Scratch3\\DeFacto_1000m_TC2000_90m_Proj.tif"
 
-# Process: Reclassify
-arcpy.gp.Reclassify_sa(EucDist_reC_RegionG2_TC2000_Proj_tif, "VALUE", "0 3000 NODATA;3000 13604 1", DeFacto_EucDist_reC_RegionG2_TC2000_Proj_tif, "DATA")
-
+#Process: Reclassify 1000m as threshold
+arcpy.gp.Reclassify_sa(EucDist_reC_RegionG2_TC2000_Proj_tif, "VALUE", "0 3000 NODATA;3000 13604 1", DeFacto_1000m_TC2000_90m_Proj_tif, "DATA")
