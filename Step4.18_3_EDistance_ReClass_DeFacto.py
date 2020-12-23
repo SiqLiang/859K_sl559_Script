a94ba5aa65178d688b9b7e92541ffd48a5c140a6
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-12-23 19:59:26 +0800

    Update Step4.18_3_EDistance_ReClass_DeFacto.py

--- Step4.18_3_EDistance_ReClass_DeFacto.py
+++ Step4.18_3_EDistance_ReClass_DeFacto.py
@@ -1,4 +1,3 @@
-
 import arcpy
 arcpy.env.overwriteOutput = True
 arcpy.env.workspace = "C:\\859K_sl559\\Scratch3"
@@ -17,4 +16,4 @@ EucDist_reC_RegionG2_TC2000_Proj_tif = "EucDist_reC_RegionG2_TC2000_Proj.tif"
 DeFacto_1000m_TC2000_90m_Proj_tif = "C:\\859K_sl559\\Scratch3\\DeFacto_1000m_TC2000_90m_Proj.tif"
 
 #Process: Reclassify 1000m as threshold
-arcpy.gp.Reclassify_sa(EucDist_reC_RegionG2_TC2000_Proj_tif, "VALUE", "0 3000 NODATA;3000 13604 1", DeFacto_1000m_TC2000_90m_Proj_tif, "DATA")
+arcpy.gp.Reclassify_sa(EucDist_reC_RegionG2_TC2000_Proj_tif, "VALUE", "0 1000 NODATA;1000 13604 1", DeFacto_1000m_TC2000_90m_Proj_tif, "DATA")
