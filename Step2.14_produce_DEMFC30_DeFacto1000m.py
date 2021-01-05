17b8f2893ad950328618238d4f0fdd47ab6eab1c
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2021-01-05 09:58:17 +0800

    Update Step2.14_produce_DEMFC30_DeFacto1000m.py

--- Step2.14_produce_DEMFC30_DeFacto1000m.py
+++ Step2.14_produce_DEMFC30_DeFacto1000m.py
@@ -1,21 +1,20 @@
 # This script aims at run raster claculator times to 
-#produce DEMFC30_PA_+scientific_name+".tif"
+#produce DEMFC30_DeFacto1000m_+scientific_name+".tif"
 
-#PA means De-jure in this script
 import arcpy
 from arcpy.sa import *
 arcpy.env.overwriteOutput = True
 arcpy.env.outputCoordinateSystem = arcpy.SpatialReference("WGS 1984")
 arcpy.env.workspace = "C:\\859K_sl559\\Scratch1"
 arcpy.env.extent = "C:\\859K_sl559\\Data\\RegionOfInterest.shp"
-DeFacto_roi= "C:\\859K_sl559\\Data\\SumUp_Dejure_Defacto\\Classified_Dejure_PA_2020Dec16.tif"
+DeFacto1000m_roi= "C:\\859K_sl559\\Data\\SumUp_Dejure_Defacto\\Reclass_DeFacto_1000m.tif"
 
 fileList = arcpy.ListRasters('ReBy_DEMFC30_*', 'All')
 len(fileList) #Check how many Reclassified rasters are avaiable now
 for fileName in fileList:
     print(fileName)
     scientific_name = fileName[13:fileName.rfind('.')]
-    outRas = Raster(fileName)*Raster(DeFacto_roi)
+    outRas = Raster(fileName)*Raster(DeFacto1000m_roi)
     outRas.save("DEMFC30_DeFacto1000m_"+scientific_name+".tif")
     print(outRas)
     
