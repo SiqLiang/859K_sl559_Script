728aa3e720698afbb9dcb7f43721371c2edc64c6
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2021-01-07 16:22:10 +0800

    Update Step5.11_1_use_os_and_Pandas_to manage_the_dataframes_2019.py

--- Step5.11_1_use_os_and_Pandas_to manage_the_dataframes_2019.py
+++ Step5.11_1_use_os_and_Pandas_to manage_the_dataframes_2019.py
@@ -5,7 +5,7 @@
 import os
 import pandas as pd
 #Set confidence threshold
-ConfidendenceThreshold= 70
+ConfidendenceThreshold= 30
 
 #Construct an empty datafram to hold all the subset dataframes
 Detailed_dfs = pd.DataFrame(columns =["Country_name","Year", "latitude","longitude","confidence","acq_date", "acq_time"]) 
@@ -14,7 +14,8 @@ CountrySummary_dfs=pd.DataFrame(columns =["Country_name","Year","NumberOfFire"])
 
 
 bathpath="C:\\859K_sl559\\Data\\ModisFire2001_2019"
-Yearlist=["2019"]
+Yearlist=["2001","2002","2003","2004","2005","2006","2007","2008","2009","2010",
+          "2011","2012","2013","2014","2015","2016","2017","2018","2019"]
 for Year in Yearlist:
     print(Year)
     path= os.path.join(bathpath,Year) 
@@ -50,16 +51,23 @@ for Year in Yearlist:
             new_row = {"Country_name":Country_name, "Year":Year, "NumberOfFire":NumberOfFire}
             CountrySummary_dfs = CountrySummary_dfs.append(new_row,ignore_index=True, sort=False)
             
+            #datafram to excel
+            Detailed_dfs= Detailed_dfs.loc[Detailed_dfs["Year"] == Year]
+            ModisFire_basepath = "C:\\859K_sl559\\Doc\\ModisFire"
+            Detailed_excel= "Detailed_"+ str(Year) +"_ConfidenceThreshold_"+str(ConfidendenceThreshold)+".xlsx"
+            path= os.path.join(ModisFire_basepath,Detailed_excel) 
+            Detailed_dfs.to_excel(path, 'Sheet1') 
+            
         else:
             os.remove(file)       
 
 #Check Detailed_dfs
-print(len(Detailed_dfs))
+#print(len(Detailed_dfs))
 #print(Detailed_dfs[0:1000])
 #print(Detailed_dfs[20000:24005])
 
 #Check CountrySummary_dfs
-print(len(CountrySummary_dfs))
+#print(len(CountrySummary_dfs))
 print(CountrySummary_dfs[-9:])
 
 ##Export CountrySummary_dfs toexcel; df.to_excel('output.xlsx', 'Sheet1')
@@ -68,23 +76,23 @@ print(CountrySummary_dfs[-9:])
 #path= os.path.join(ModisFire_basepath,CountrySummary_excel) 
 #CountrySummary_dfs.to_excel(path, 'Raw') 
 
-#Export Detailed_dfs toexcel; df.to_excel('output.xlsx', 'Sheet1')
-ModisFire_basepath = "C:\\859K_sl559\\Doc\\ModisFire"
-Detailed_excel= "Detailed_"+"ConfidenceThreshold_"+str(ConfidendenceThreshold)+".xlsx"
-path= os.path.join(ModisFire_basepath,Detailed_excel) 
-Detailed_dfs.to_excel(path, 'Raw') 
+###Export Detailed_dfs toexcel; df.to_excel('output.xlsx', 'Sheet1')
+#ModisFire_basepath = "C:\\859K_sl559\\Doc\\ModisFire"
+#Detailed_excel= "Detailed_"+"ConfidenceThreshold_"+str(ConfidendenceThreshold)+".xlsx"
+#path= os.path.join(ModisFire_basepath,Detailed_excel) 
+#Detailed_dfs.to_excel(path, 'Raw') 
 
-#Export Detailed_dfs in 2003 toexcel; df.to_excel('output.xlsx', 'Sheet1')
+###Export Detailed_dfs in 2003 toexcel; df.to_excel('output.xlsx', 'Sheet1')
 #Detailed_2003_dfs= Detailed_dfs.loc[Detailed_dfs["Year"] == "2003"]
 #ModisFire_basepath = "C:\\859K_sl559\\Doc\\ModisFire"
 #Detailed_2003_excel= "Detailed_2003_"+"ConfidenceThreshold_"+str(ConfidendenceThreshold)+".xlsx"
 #path= os.path.join(ModisFire_basepath,Detailed_2003_excel) 
 #Detailed_2003_dfs.to_excel(path, 'Sheet1') 
 
-#Export Detailed_dfs in 2019 toexcel; df.to_excel('output.xlsx', 'Sheet1')
-Detailed_2019_dfs= Detailed_dfs.loc[Detailed_dfs["Year"] == "2019"]
-ModisFire_basepath = "C:\\859K_sl559\\Doc\\ModisFire"
-Detailed_2019_excel= "Detailed_2019_"+"ConfidenceThreshold_"+str(ConfidendenceThreshold)+".xlsx"
-path= os.path.join(ModisFire_basepath,Detailed_2019_excel) 
-Detailed_2019_dfs.to_excel(path, 'Sheet1') 
+###Export Detailed_dfs in 2019 toexcel; df.to_excel('output.xlsx', 'Sheet1')
+#Detailed_2019_dfs= Detailed_dfs.loc[Detailed_dfs["Year"] == "2019"]
+#ModisFire_basepath = "C:\\859K_sl559\\Doc\\ModisFire"
+#Detailed_2019_excel= "Detailed_2019_"+"ConfidenceThreshold_"+str(ConfidendenceThreshold)+".xlsx"
+#path= os.path.join(ModisFire_basepath,Detailed_2019_excel) 
+#Detailed_2019_dfs.to_excel(path, 'Sheet1') 
 
