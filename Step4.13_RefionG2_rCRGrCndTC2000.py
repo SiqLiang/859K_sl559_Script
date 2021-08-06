257010411b485797a746f6bfe32300c1f1569a68
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2021-08-06 10:34:05 +0800

    Update Step4.13_RefionG2_rCRGrCndTC2000.py

--- Step4.13_RefionG2_rCRGrCndTC2000.py
+++ Step4.13_RefionG2_rCRGrCndTC2000.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Dec  4 22:20:29 2020
+Created on 2021Aug6
 
 @author: sl559
-I have regiongrouped the ReclassNoData_H_TC2000.tif file to
-RegionG_rCNoDataTC2000_test2.tif, and classified the clusters with 
+I have regiongrouped the FC2000_MtoNR_rC_FC30_ROIexpanded.tif file to
+RegionG_FC2000_MtoNR_rC_FC30_ROIexpanded.tif, and classified the clusters with 
 cellsize smaller than 113 to nodata, the result is
-Reclass_RegionG_rCNTC2000.tif. 
+Reclass_RegionG_FC2000_ROIexpanded.tif 
 Now, I run the 2nd regiongroup to sign a sequantial name to each cluster.
 """
 #2020Dec4
@@ -14,14 +14,13 @@ Now, I run the 2nd regiongroup to sign a sequantial name to each cluster.
 import arcpy
 import os
 arcpy.env.overwriteOutput = True
-arcpy.env.workspace = "C:\\859K_sl559\\Scratch3"
-arcpy.env.extent = "C:\\859K_sl559\\Scratch3\\ReclassNoData_H_TC2000.tif"
-print (os.path.exists("C:\\859K_sl559\\Scratch3\\ReclassNoData_H_TC2000.tif"))
-print (os.path.exists("C:\\859K_sl559\\Scratch3\\Reclass_RegionG_rCNTC2000.tif"))
-# Local variables:
-Reclass_RegionG_rCNTC2000_tif = "C:\\859K_sl559\\Scratch3\\Reclass_RegionG_rCNTC2000.tif"
-RegionG2_rCRGrCndTC2000_tif = "C:\\859K_sl559\\Scratch3\\RegionG2_rCRGrCndTC2000.tif"
-# Process: Region Group
-arcpy.gp.RegionGroup_sa(Reclass_RegionG_rCNTC2000_tif, RegionG2_rCRGrCndTC2000_tif, "FOUR", "WITHIN", "ADD_LINK", "")
+arcpy.env.workspace = "C:\\859K_sl559\\Data\\ForestCover2000"
+arcpy.env.extent = "C:\\859K_sl559\\Data\\ROI\\RegionOfInterest_expanded.shp"
 
-print (os.path.exists("C:\\859K_sl559\\Scratch3\\RegionG2_rCRGrCndTC2000.tif"))
+# Local variables:
+Reclass_RegionG_FC2000_tif = "C:\\859K_sl559\\Data\\ForestCover2000\\Reclass_RegionG_FC2000_ROIexpanded.tif"
+RegionG2_rCRGrCndTC2000_tif = "C:\\859K_sl559\\Scratch3\\RegionG2_rCRGrCndTC2000_ROIexpanded.tif"
+# Process: Region Group
+arcpy.gp.RegionGroup_sa(Reclass_RegionG_FC2000_tif, RegionG2_rCRGrCndTC2000_tif, "FOUR", "WITHIN", "ADD_LINK", "")
+
+print (os.path.exists("C:\\859K_sl559\\Scratch3\\RegionG2_rCRGrCndTC2000_ROIexpanded.tif"))
