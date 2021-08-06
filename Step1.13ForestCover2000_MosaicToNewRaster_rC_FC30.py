319c2d32b3144f9e40ac774c3b8025b4d1143aad
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2021-08-06 10:12:09 +0800

    Update Step1.13ForestCover2000_MosaicToNewRaster_rC_FC30.py

--- Step1.13ForestCover2000_MosaicToNewRaster_rC_FC30.py
+++ Step1.13ForestCover2000_MosaicToNewRaster_rC_FC30.py
@@ -13,18 +13,18 @@ arcpy.env.overwriteOutput = True
 
 # Set the analysis environments
 arcpy.env.workspace = "C:\\859K_sl559\\Data\\ForestCover2000"
-arcpy.env.extent = "C:\\859K_sl559\\Data\\RegionOfInterest.shp"
+arcpy.env.extent = "C:\\859K_sl559\\Data\\ROI\\RegionOfInterest_expanded.shp"
 arcpy.env.outputCoordinateSystem = arcpy.SpatialReference("WGS 1984")
-cellSize = 0.0008333
+cellSize = 0.000833333333333333 #90m
 
 # Local variables:
-SixForestCover2000_MosaictoNewRater_tif = "C:\\859K_sl559\\Data\\ForestCover2000\\SixForestCover2000_MosaictoNewRater.tif"
-SixFC2000_MtoNR_rC_FC30_tif= "C:\\859K_sl559\\Data\\ForestCover2000\\SixFC2000_MtoNR_rC_FC30.tif"
+ForestCover2000_MosaictoNewRater_tif = "C:\\859K_sl559\\Data\\ForestCover2000\\ForestCover2000_MosaictoNewRater_ROIexpanded.tif"
+FC2000_MtoNR_rC_FC30_tif= "C:\\859K_sl559\\Data\\ForestCover2000\\FC2000_MtoNR_rC_FC30_ROIexpanded.tif"
 #Execute Reclassify 
 reclassField = "Value"
 #for projection World_Eckert_IV
 #cellsize= 94.126759784775m*94.126759784775m/1000000 = 0.88598469075807 ha
 #100ha/cellsize=112.87  ; patch with 113cell is bigger than 100ha
 remap = "0 30 NODATA;30 100 1; NODATA NODATA"
-arcpy.gp.Reclassify_sa(SixForestCover2000_MosaictoNewRater_tif, reclassField, remap, SixFC2000_MtoNR_rC_FC30_tif)
+arcpy.gp.Reclassify_sa(ForestCover2000_MosaictoNewRater_tif, reclassField, remap, FC2000_MtoNR_rC_FC30_tif)
 #run successfully, however the output cellsize is still 0.00025 instead of the mentioned 0.0008333
