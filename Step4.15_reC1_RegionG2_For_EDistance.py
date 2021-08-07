4b80e3e45e204f25ef7e43d6ddc9ff0f247f3e74
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2021-08-07 09:58:37 +0800

    Update Step4.15_reC1_RegionG2_For_EDistance.py

--- Step4.15_reC1_RegionG2_For_EDistance.py
+++ Step4.15_reC1_RegionG2_For_EDistance.py
@@ -5,16 +5,16 @@ from arcpy.sa import *
 arcpy.env.overwriteOutput = True
 
 # Set the analysis environments
-arcpy.env.workspace = "C:\\859K_sl559\\Scratch3"
-arcpy.env.extent = "C:\\859K_sl559\\Data\\RegionOfInterest.shp"
+arcpy.env.workspace = "C:\\859K_sl559\\Scratch_DistanceToEdge"
+arcpy.env.extent = "C:\\859K_sl559\\Data\\ROI\\RegionOfInterest_expanded.shp"
 
 # Local variables:
-RegionG2_rCRGrCndTC2000_Proj_tif= "C:\\859K_sl559\\Scratch3\\RegionG2_rCRGrCndTC2000_Proj.tif"
-reC1_RegionG2_TC2000_Proj_tif= "C:\\859K_sl559\\Scratch3\\reC1_RegionG2_TC2000_Proj.tif"
+RegionG2_rCRGrCndTC2000_Proj_tif="C:\\859K_sl559\\Scratch_DistanceToEdge\\Projected_RegionG2_Reclass_RegionG_FC2000_ROIexpanded_300m.tif"
+ReC2_RegionG2_TC2000_Proj_tif= "C:\\859K_sl559\\Scratch_DistanceToEdge\\ReC2_Proj_RegionG2_ReC1_RegionG1_FC2000_ROIexpanded_300m.tif"
 #Execute Reclassify 
 reclassField = "Count"
 #for projection World_Eckert_IV
 #cellsize= 94.126759784775m*94.126759784775m/1000000 = 0.88598469075807 ha
 #100ha/cellsize=112.87  ; patch with 113cell is bigger than 100ha
-remap = "113 63623690 NODATA; NODATA 1"
-arcpy.gp.Reclassify_sa(RegionG2_rCRGrCndTC2000_Proj_tif, reclassField, remap, reC1_RegionG2_TC2000_Proj_tif)
+remap = "9 9904221 NODATA; NODATA 1"
+arcpy.gp.Reclassify_sa(RegionG2_rCRGrCndTC2000_Proj_tif, reclassField, remap, ReC2_RegionG2_TC2000_Proj_tif)
