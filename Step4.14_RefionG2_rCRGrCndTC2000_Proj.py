daa560cd15a94933493afb5d2977ceef3c52dc89
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2021-08-07 09:58:31 +0800

    Update Step4.14_RefionG2_rCRGrCndTC2000_Proj.py

--- Step4.14_RefionG2_rCRGrCndTC2000_Proj.py
+++ Step4.14_RefionG2_rCRGrCndTC2000_Proj.py
@@ -6,7 +6,7 @@ arcpy.env.workspace = "C:\\859K_sl559\\Scratch_DistanceToEdge"
 arcpy.env.extent = "C:\\859K_sl559\\Data\\ROI\\RegionOfInterest_expanded.shp"
 # Local variables:
 RegionG2_rCRGrCndTC2000_tif = "C:\\859K_sl559\\Scratch_DistanceToEdge\\RegionG2_Reclass_RegionG_FC2000_ROIexpanded_300m.tif"
-RegionG2_rCRGrCndTC2000_Proj_tif="C:\\859K_sl559\\Scratch_DistanceToEdge\\Projected_RegionG2_Reclass_RegionG_FC2000_ROIexpanded_300m_py.tif"
+RegionG2_rCRGrCndTC2000_Proj_tif="C:\\859K_sl559\\Scratch_DistanceToEdge\\Projected_RegionG2_Reclass_RegionG_FC2000_ROIexpanded_300m.tif"
 # Process: Project
 arcpy.ProjectRaster_management(RegionG2_rCRGrCndTC2000_tif, RegionG2_rCRGrCndTC2000_Proj_tif, "PROJCS['World_Eckert_IV',GEOGCS['GCS_WGS_1984',DATUM['D_WGS_1984',SPHEROID['WGS_1984',6378137.0,298.257223563]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Eckert_IV'],PARAMETER['False_Easting',0.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',0.0],UNIT['Meter',1.0]]", "NEAREST", "321.977497514159 321.977497514159", "", "", "GEOGCS['GCS_WGS_1984',DATUM['D_WGS_1984',SPHEROID['WGS_1984',6378137.0,298.257223563]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]")
 
