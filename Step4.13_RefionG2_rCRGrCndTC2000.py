25ec6de578acc1f74cc4eefb53465ca30c1cd65c
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2021-08-07 11:18:52 +0800

    Update Step4.13_RefionG2_rCRGrCndTC2000.py

--- Step4.13_RefionG2_rCRGrCndTC2000.py
+++ Step4.13_RefionG2_rCRGrCndTC2000.py
@@ -24,4 +24,20 @@ RegionG2_rCRGrCndTC2000_tif = "C:\\859K_sl559\\Scratch_DistanceToEdge\\RegionG2_
 # Process: Region Group
 arcpy.gp.RegionGroup_sa(Reclass_RegionG_FC2000_tif, RegionG2_rCRGrCndTC2000_tif, "FOUR", "WITHIN", "ADD_LINK", "")
 
+#---------
+# Local variables:
+RegionG2_tif = "C:\\859K_sl559\\Scratch_DistanceToEdge\\RegionG2_Reclass_RegionG_FC2000_ROIexpanded_300m.tif"
+ReC2_RegionG2_tif= "C:\\859K_sl559\\Scratch_DistanceToEdge\\ReC2_RegionG2_ReC1_RegionG1_FC2000_ROIexpanded_300m.tif"
+#Execute Reclassify 
+reclassField = "Count"
+#for projection World_Eckert_IV
+#cellsize= 94.126759784775m*94.126759784775m/1000000 = 0.88598469075807 ha
+#100ha/cellsize=112.87  ; patch with 113cell is bigger than 100ha
+remap = "9 9904221 NODATA; NODATA 1"
+arcpy.gp.Reclassify_sa(RegionG2_tif, reclassField, remap, ReC2_RegionG2_tif)
+#---------
 
+#--------
+#I conducted raster projection in arcmap pro with World Eckert Iv
+# I also conducted EDistance in arcmap pro; distance method:Planar
+#-------
